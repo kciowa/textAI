a6f9f1a535ea5e2790f6ce053b2a0b3df3120870
Kseniya Dyosheva <76409598+kciowa@users.noreply.github.com>
2021-11-03 03:36:17 +0300

    change arr size

--- plagiarism.cpp
+++ plagiarism.cpp
@@ -30,7 +30,7 @@ using std::cout;
 using std::endl;
 using namespace std;
 
-const int N = 5000;
+const int N = 1500;
 
 double antiPlagiarism(string text, string fragment);
 int getNewArrSize(string* mas, string text);
